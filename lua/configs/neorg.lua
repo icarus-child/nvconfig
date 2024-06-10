2e2072becdfcf1ead3a1c92fc92e2195da8519bb
Ethan Meier <e652@hotmail.ca>
2024-06-10 00:28:08 -0700

    neorg highlighting

--- lua/configs/neorg.lua
+++ lua/configs/neorg.lua
@@ -6,7 +6,41 @@ require("neorg").setup {
         icon_preset = "diamond"
       }
     },
-    ["core.highlights"] = {},
+    ["core.highlights"] = {
+      config = {
+        highlights = {
+          headings = {
+            {
+              prefix = "guifg=#f57a16",
+              title = "guifg=#f57a16"
+            },
+            {
+              prefix = "+@label",
+              title = "+@label"
+            },
+            {
+              prefix = "+@constant",
+              title = "+@constant"
+            },
+            {
+              prefix = "+@string",
+              title = "+@string"
+            },
+            {
+              prefix = "+@label",
+              title = "+@label"
+            },
+            {
+              prefix = "+@constructor",
+              title = "+@constructor"
+            },
+          },
+          links = {
+            file = "gui=undercurl"
+          },
+        },
+      }
+    },
     ["core.dirman"] = {
       config = {
         workspaces = {
