b997a926a859310bcf64178dc064de78cbeb905c
Ethan Meier <e652@hotmail.ca>
2024-06-17 23:16:25 -0700

    add journal

--- lua/configs/neorg.lua
+++ lua/configs/neorg.lua
@@ -56,6 +56,9 @@ require("neorg").setup {
         engine = "nvim-cmp"
       }
     },
+    ["core.journal"] = {
+      strategy = "flat"
+    },
   },
 }
 
--- lua/options.lua
+++ lua/options.lua
@@ -3,11 +3,8 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-local g = vim.g
 
 o.cursorlineopt = 'both' -- to enable cursorline!
-
-g.maplocalleader = '\\'
 
 -- o.foldmethod = 'fold-expr'
 -- o.foldexpr = 'nvim_treesitter#foldexpr()'
