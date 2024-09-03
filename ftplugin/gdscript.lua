7db9d7a0aa89e27dfe8557c38f62720266cd0924
Ethan Meier <e652@hotmail.ca>
2024-09-02 21:04:45 -0700

    gdscript and conform

--- ftplugin/gdscript.lua
+++ ftplugin/gdscript.lua
@@ -1,18 +1,6 @@
-local ip_address = '127.0.0.1'
-
-local port = os.getenv('GDScript_Port') or '6005'
-local cmd = vim.lsp.rpc.connect(ip_address, tonumber(port))
-local pipe = '/tmp/godot.pipe'
-
-vim.lsp.start({
-    name = 'Godot',
-    cmd = cmd,
-    filetypes = {'gdscript'},
-    root_dir = vim.fs.dirname(vim.fs.find({'project.godot', '.git'}, {
-        upward = true,
-        path = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
-    })[1]),
-    on_attach = function(client, bufnr)
-        vim.api.nvim_command('echo serverstart("' .. pipe .. '")')
-    end
-})
+vim.o.expandtab = false
+vim.o.copyindent = true
+vim.o.preserveindent = true
+vim.o.softtabstop = 0
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -45,10 +45,12 @@ return {
         "html-lsp",
         "css-lsp",
         "prettier",
+        "prettierd",
         "bash-language-server",
         "tsserver",
         -- "eslint-lsp",
         "python-lsp-server",
+        "gdtoolkit",
       },
     },
   },
@@ -167,4 +169,40 @@ return {
       require "configs.harpoon"
     end,
   },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      require "configs.conform"
+    },
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+    config = function()
+      local lint = require("lint")
+
+      lint.linters_by_ft = {
+        gdscript = { "gdlint" }
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+          group = lint_augroup,
+          callback = function()
+            lint.try_lint()
+          end,
+      })
+
+      vim.keymap.set("n", "<leader>lt", function()
+        lint.try_lint()
+      end, { desc = "Trigger linting for current file" })
+    end
+  },
+
 }
