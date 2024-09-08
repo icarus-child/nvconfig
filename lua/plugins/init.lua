3caa560f09419f6d07047205be870b6704f34d3c
Ethan Meier <e652@hotmail.ca>
2024-09-08 15:07:56 -0700

    format on save reworked

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,7 +20,7 @@ return {
 
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
 
