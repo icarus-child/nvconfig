8fb5008e31d776b25b5b6477337511a1ffc9329e
Ethan Meier <e652@hotmail.ca>
2025-04-27 00:07:08 -0700

    ctrl right arrow is copilot accept keybind

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -343,7 +343,7 @@ return {
     config = function()
       vim.g.copilot_no_tab_map = true
       vim.g.copilot_assume_mapped = true
-      vim.keymap.set("i", "<Right>", "copilot#Accept()", { expr = true, silent = true, replace_keycodes = false })
+      vim.keymap.set("i", "<C-Right>", "copilot#Accept()", { expr = true, silent = true, replace_keycodes = false })
     end,
   },
 }
