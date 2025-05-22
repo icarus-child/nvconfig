80133f26765db6e01f5c7c7acdba945f1ca3fc91
Ethan Meier <e652@hotmail.ca>
2025-05-22 16:55:01 -0700

    molten

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -185,14 +185,15 @@ return {
         local quarto_cfg = require("quarto.config").config
         quarto_cfg.codeRunner.default_method = "molten"
 
-        local venv = os.getenv "VIRTUAL_ENV" or os.getenv "CONDA_PREFIX"
-        if venv ~= nil then
-          -- in the form of /home/benlubas/.virtualenvs/VENV_NAME
-          venv = string.match(venv, "/.+/(.+)")
-          vim.cmd(("MoltenInit %s"):format(venv))
-        else
-          vim.cmd "MoltenInit python3"
-        end
+        -- local venv = os.getenv "VIRTUAL_ENV" or os.getenv "CONDA_PREFIX"
+        -- if venv ~= nil then
+        --   -- in the form of /home/benlubas/.virtualenvs/VENV_NAME
+        --   venv = string.match(venv, "/.+/(.+)")
+        --   vim.cmd(("MoltenInit %s"):format(venv))
+        -- else
+        --   vim.cmd "MoltenInit python3"
+        -- end
+        vim.cmd [[MoltenInit]]
       end
       local deinit = function()
         local quarto_cfg = require("quarto.config").config
